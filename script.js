b1de92637961a4c3f2f0c9c714e66a1e7937ee72
sumayiaakter <aktersumayia92@gmail.com>
2024-10-09 15:47:54 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function nickname(){
+    document.getElementById("name").innerHTML ="My nickname is Ratry"
+}
+
+function myimage() {
+   document.getElementById('myimage').src='image/image/sumayia-1.jpg'
+  }
+
+function image(){
+    document.getElementById('myimage').src='image/sumayia-2.jpg'
+}
+
+function mystyle(){
+   document.getElementById('style').style.color="green";
+}
+
+function hidetext(){
+    document.getElementById('hide').style.display='none'
+}
+function showtext(){
+    document.getElementById('show').style.display='block'
+}
